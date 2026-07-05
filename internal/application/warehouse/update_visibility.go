cded341344672e9f9a233e746782526ff012bf64
Erfangh-80 <erfangh4871gholami@gmail.com>
2026-07-05 15:16:32 +0330

    feat: add UpdateWarehouseVisibility use case

---
+++ internal/application/warehouse/update_visibility.go
@@ -0,0 +1,26 @@
+package warehouse
+
+import (
+	"stock-service/internal/domain/warehouse"
+)
+
+type UpdateVisibilityUseCase struct {
+	repo warehouse.Repository
+}
+
+func NewUpdateVisibilityUseCase(repo warehouse.Repository) *UpdateVisibilityUseCase {
+	return &UpdateVisibilityUseCase{repo: repo}
+}
+
+func (uc *UpdateVisibilityUseCase) Execute(warehouseID int64, isPublic bool) error {
+	w, err := uc.repo.FindByID(warehouseID)
+	if err != nil {
+		return err
+	}
+	if isPublic {
+		w.MakePublic()
+	} else {
+		w.MakePrivate()
+	}
+	return uc.repo.Save(w)
+}
